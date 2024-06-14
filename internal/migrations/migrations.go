d4e8a3f605b3f87ad2fbc4255689126f66b50ade
Deepak Rathod <2021bcs511@sggs.ac.in>
2024-06-14 06:30:08 +0530

    Created migrations package to auto-migrate using GORM

---
+++ internal/migrations/migrations.go
@@ -0,0 +1,94 @@
+/**
+ * Migrations
+ * 
+ * Models may look redundant
+ * they declared again to remove complexity,
+ * and they will be merged with logical models later
+ * if required
+*/
+package migrations;
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+type User struct {
+	ID          int64     `gorm:"primaryKey;autoIncrement"`
+	PhoneNumber string    `gorm:"unique;not null"`
+	OTP         string    `gorm:"default:null"`
+	CreatedAt   time.Time `gorm:"default:now()"`
+	Forms       []Form    `gorm:"foreignKey:UserID"`
+	Sessions    []Session `gorm:"foreignKey:UserID"`
+	Submissions []Submission `gorm:"foreignKey:UserID"`
+}
+
+type Form struct {
+	ID        int64     `gorm:"primaryKey;autoIncrement"`
+	UserID    int64     `gorm:"not null"`
+	Title     string    `gorm:"not null"`
+	LinkCode  string    `gorm:"default:null"`
+	Timeout   int       `gorm:"default:null"`
+	CreatedAt time.Time `gorm:"default:now()"`
+	IsActive  bool      `gorm:"default:true"`
+	Pages     []Page    `gorm:"foreignKey:FormID"`
+	Submissions []Submission `gorm:"foreignKey:FormID"`
+}
+
+type Page struct {
+	ID            int64     `gorm:"primaryKey;autoIncrement"`
+	FormID        int64     `gorm:"not null"`
+	Title         string    `gorm:"not null"`
+	RelativeOrder int16     `gorm:"default:0"`
+	IsActive      bool      `gorm:"default:true"`
+	Questions     []Question `gorm:"foreignKey:PageID"`
+}
+
+type Question struct {
+	ID            int64     `gorm:"primaryKey;autoIncrement"`
+	PageID        int64     `gorm:"not null"`
+	RelativeOrder int16     `gorm:"default:0"`
+	QuestionText  string    `gorm:"not null"`
+	QuestionType  string    `gorm:"not null"`
+	Validations   schema.JSON `gorm:"default:'{}'"`
+	Config        schema.JSON `gorm:"default:'{}'"`
+	IsActive      bool      `gorm:"default:true"`
+	SubmissionAnswers []SubmissionAnswer `gorm:"foreignKey:QuestionID"`
+}
+
+type Submission struct {
+	ID         int64     `gorm:"primaryKey;autoIncrement"`
+	FormID     int64     `gorm:"not null"`
+	UserID     *int64    `gorm:"default:null"`  // Optional, so use a pointer
+	CreatedAt  time.Time `gorm:"default:now()"`
+	UpdatedAt  time.Time `gorm:"default:now()"`
+	Submitted  bool      `gorm:"default:false"`
+	SubmissionAnswers []SubmissionAnswer `gorm:"foreignKey:SubmissionID"`
+}
+
+type SubmissionAnswer struct {
+	ID           int64     `gorm:"primaryKey;autoIncrement"`
+	SubmissionID int64     `gorm:"not null"`
+	QuestionID   int64     `gorm:"not null"`
+	Answer       schema.JSON `gorm:"not null"`
+}
+
+type Session struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	UserID    int64     `gorm:"not null"`
+	IsActive  bool      `gorm:"default:true"`
+	CreatedAt time.Time `gorm:"default:now()"`
+}
+
+/**
+ * Create Tables and Updates tables for database
+*/ 
+func StartAutoMigrate() {
+	db := dbm.GetConnection();
+	// Migrate the schema
+	db.AutoMigrate(&User{}, &Form{}, &Page{}, &Question{}, &Submission{}, &SubmissionAnswer{}, &Session{})
+}
